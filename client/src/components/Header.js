a2c303d507d2e3699f7bbb0aa763310eae3c6ffe
KowshikI87 <kowshikislam8@gmail.com>
2023-01-31 14:12:13 -0500

    convert product and header to JSX

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+const Product = ({ total, disabled }) => {
+  return (
+    <header>
+      <h1>The Shop!</h1>
+      <div className="cart">
+        <h2>Your Cart</h2>
+        <p>Your cart is empty</p>
+        <p>Total: {total}</p>
+        <a className={`button checkout ${disabled ? 'disabled' : ''}`}>Checkout</a>
+      </div>
+    </header>
+  )
+}
+
+export default Product
