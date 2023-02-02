2beb428b3191068cf4552ae783d63f064374b73a
Matthew-D-Johnston <49563465+Matthew-D-Johnston@users.noreply.github.com>
2023-02-02 14:55:50 -0400

    Update AddForm.js

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,35 +1,64 @@
+import axios from 'axios';
 import { useState } from 'react';
 
-const InputGroup = ({ htmlFor, name, id }) => {
+const InputGroup = ({ htmlFor, name, id, onChange, inputValue }) => {
   return (
     <div className="input-group">
       <label htmlFor={htmlFor}>{name}</label>
-      <input type="text" id={id} value="" />
+      <input type="text" id={id} value={inputValue} onChange={onChange} />
     </div>
   )
 }
 
 const AddForm = () => {
   const [productName, setProductName] = useState('');
-  const [price, setPrice] = useState(0);
+  const [productPrice, setProductPrice] = useState('');
+  const [productQuantity, setProductQuantity] = useState('');
 
-  const handleAddAProductClick = (e) => {
+  const handleAddAProductDisplayClick = (e) => {
     e.preventDefault();
     const addFormDiv = e.target.closest('div');
     addFormDiv.classList.toggle("add-form");
     console.log(addFormDiv);
   }
 
+  const handleNameChange = (e) => {
+    setProductName(e.target.value);
+  }
+
+  const handlePriceChange = (e) => {
+    setProductPrice(e.target.value);
+  }
+
+  const handleQuantityChange = (e) => {
+    setProductQuantity(e.target.value);
+  }
+
+  const handleAddProductClick = async (e) => {
+    e.preventDefault();
+    let title = productName;
+    let price = parseFloat(productPrice);
+    let quantity = parseInt(productQuantity, 10);
+    let product = { title, price, quantity };
+
+    try {
+      const response = await axios.post('/api/products', product);
+      console.log({response});
+    } catch (error) {
+      console.error('Error adding new product.')
+    }
+  }
+
   return (
     <div className="add-form">
-      <p><a className="button add-product-button" onClick={handleAddAProductClick}>Add A Product</a></p>
+      <p><a className="button add-product-button" onClick={handleAddAProductDisplayClick}>Add A Product</a></p>
       <h3>Add Product</h3>
       <form>
-        <InputGroup htmlFor="product-name" name="Product Name" id="product-name" />
-        <InputGroup htmlFor="product-price" name="Price" id="product-price" />
-        <InputGroup htmlFor="product-quantity" name="Quantity" id="product-quantity" />
+        <InputGroup htmlFor="product-name" name="Product Name" id="product-name" onChange={handleNameChange} inputValue={productName} />
+        <InputGroup htmlFor="product-price" name="Price" id="product-price" onChange={handlePriceChange} inputValue={productPrice} />
+        <InputGroup htmlFor="product-quantity" name="Quantity" id="product-quantity" onChange={handleQuantityChange} inputValue={productQuantity} />
         <div className="actions form-actions">
-          <a className="button">Add</a>
+          <a className="button" onClick={handleAddProductClick}>Add</a>
           <a className="button">Cancel</a>
         </div>
       </form>
