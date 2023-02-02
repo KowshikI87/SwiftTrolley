e76e7605689fa36bc410ec2ee82d1b85fb37a178
KowshikI87 <kowshikislam8@gmail.com>
2023-02-02 12:46:22 -0500

    cart total implemented

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-const Product = ({ productId, description, price, quantity, disabled, onDelete}) => {
-  const handleAddToCartClick = async () => {
-    console.log(productId);
-    await axios.post("/api/add-to-cart", { productId })
-  }
-
+const Product = ({ productId, description, price, quantity, disabled, onDelete, onAddToCart}) => {
   return (
     <div className="product">
       <div className="product-details">
@@ -13,7 +8,7 @@ const Product = ({ productId, description, price, quantity, disabled, onDelete})
         <p className="price">{price}</p>
         <p className="quantity">{quantity} left in stock</p>
         <div className="actions product-actions">
-          <a className={`button add-to-cart ${disabled ? 'disabled' : ''}`} onClick={handleAddToCartClick}>Add to Cart</a>
+          <a className={`button add-to-cart ${disabled ? 'disabled' : ''}`} onClick={onAddToCart}>Add to Cart</a>
           <a className="button edit">Edit</a>
         </div>
         <a className="delete-button" onClick={() => onDelete(productId)}><span>X</span></a>
